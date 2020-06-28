ce48291714361698c192ad7c8b709a557ca6f1ce
Cristiano Almeida <cicbboyzb@gmail.com>
2020-06-28 01:10:03 -0300

    Correcao do BFF devido remocao de dependencia

--- bff-istio/src/main/java/br/com/votorantim/istio/backend/configuration/AppConfiguration.java
+++ bff-istio/src/main/java/br/com/votorantim/istio/backend/configuration/AppConfiguration.java
@@ -1,15 +1,8 @@
 package br.com.votorantim.istio.backend.configuration;
 
-import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
-import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
-import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
-import org.springframework.cloud.client.circuitbreaker.CircuitBreaker;
-import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
-import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
